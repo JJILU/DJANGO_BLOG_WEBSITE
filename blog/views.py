4663c67499e2ad6b6c8c1e5e553813382b8f5313
naomixox <walu9963@gmail.com>
2025-02-21 12:13:13 +0200

    first commit

---
+++ blog/views.py
@@ -0,0 +1,15 @@
+from django.utils.timezone import now
+from django.shortcuts import render
+
+# Create your views here.
+def starting_page(request):
+    print(f"Time-stamp ==>> {now().timestamp()}")
+    return render(request, "blog/index.html", {'timestamp': now().timestamp()})
+    
+
+def posts(request):
+    return render(request, "blog/all-posts.html", {'timestamp': now().timestamp()})
+    
+
+def post_detail(request):
+    return render(request, "blog/post-details.html", {'timestamp': now().timestamp()})
